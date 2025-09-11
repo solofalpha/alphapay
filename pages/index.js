0c39b426420f6dd3f76223868906d71780b42722
KingsleyCodes <57971493+KingsleyCodes@users.noreply.github.com>
2025-09-11 01:11:11 +0100

    Next and tailwind working
    Commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-green-900 via-yellow-800 to-blue-400">
+      <h1 className="text-5xl font-bold text-white">
+        🚀 Tailwind is Working! This is Terrific!
+      </h1>
+    </div>
+  )
+}
